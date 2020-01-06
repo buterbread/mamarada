ed54f3eff6b96e29dd7c4913899d8fdbe6faf99f
fara <buterbread@gmail.com>
2020-01-06 19:44:56 +0200

    Setup #1

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
-export function createStore () {
-            return new Vuex.Store({
-  state () {
-            return {
-  }
-          },
-          mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
-          }
+export function createStore() {
+  return new Vuex.Store({
+    state() {
+      return {}
+    },
+    mutations: {},
+    actions: {},
+    modules: {}
+  })
+}
